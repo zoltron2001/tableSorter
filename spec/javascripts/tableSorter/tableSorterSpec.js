b532644795588efc9ac03846ae2cac8a6d35be00
Zollie Barnes <zolliebarnes@gmail.com>
2017-02-12 03:15:05 -0600

    add partition tests

--- spec/javascripts/tableSorter/tableSorterSpec.js
+++ spec/javascripts/tableSorter/tableSorterSpec.js
@@ -28,32 +28,67 @@ describe("TableSorter", function() {
   });
 
   // quicksort: function(array) {
-  //   var partition = TableSorter.partition(array);
+  //   var partition   = TableSorter.partition(array);
+  //   // recurse
+  //   partition.left  = TableSorter.sort(partition.left);
+  //   partition.right = TableSorter.sort(partition.right); //
   //   return TableSorter.reconstructArray(partition);
   // },
 
   describe("partition", function() {
-
-    it("", function() {
+    describe("return object", function() {
+      var empty;
+      beforeEach(function() {
+        empty = TableSorter.partition([]);
+      });
+      it("has left attribute", function() {
+        expect(empty.left).toBeDefined();
+      });
+      it("has middle attribute", function() {
+        expect(empty.middle).toBeDefined();
+      });
+      it("has right attribute", function() {
+        expect(empty.left).toBeDefined();
+      });
+    });
+    describe("partitioning", function() {
+      describe("middle",function() {
+        var middled;
+        beforeEach(function() {
+          middled = TableSorter.partition([1,1,1]);
+        });
+        it("middle has elements", function() {
+          expect(middled.middle).toEqual([1,1,1]);
+        });
+        it("left empty", function() {
+          expect(middled.left).toEqual([]);
+        });
+        it("right empty", function() {
+          expect(middled.right).toEqual([]);
+        });
+      });
+      describe("left/right",function() {
+        var partition,left,right;
+        beforeEach(function() {
+          var lopsided = [0,1];
+          partition    = TableSorter.partition(lopsided);
+          left         = partition.left;
+          right        = partition.right;
+        });
+        it("at least one is empty", function() {
+          leftEmpty  = left[0]  == undefined;
+          rightEmpty = right[0] == undefined;
+          expect( leftEmpty && rightEmpty ).not.toBeTruthy();
+        });
+        it("only one has element", function() {
+          leftHasElement  = left.length  == 1
+          rightHasElement = right.length == 1
+          expect(leftHasElement && rightHasElement).toEqual(false);
+        });
+      });
     });
 
   });
-
-  // partition: function(array) {
-  //   var pivot  = array[ TableSorter.randomInteger(length) ],
-  //       left   = [],
-  //       middle = [],
-  //       right  = [];
-  //   for (var i = 0; i < length; i++) {
-  //     if      ( array[i] <  pivot ) {   left.push(array[i]) }
-  //     else if ( array[i] == pivot ) { middle.push(array[i]) }
-  //     else if ( array[i] >  pivot ) {  right.push(array[i]) }
-  //   }
-  //   // recurse
-  //   left  = TableSorter.sort(left);
-  //   right = TableSorter.sort(right);
-  //   return {left: left, middle: middle, right: right}
-  // },
 
   describe("reconstructArray", function() {
     it("returns expected", function() {
