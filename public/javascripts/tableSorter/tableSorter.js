71c883d2b9b73189394c6fd4ae68e6c0ff0db643
Zollie Barnes <zolliebarnes@gmail.com>
2017-02-23 13:45:27 -0600

    add TableSorter

--- public/javascripts/tableSorter/tableSorter.js
+++ public/javascripts/tableSorter/tableSorter.js
@@ -1,5 +1,12 @@
 var TableSorter = {
-  sort: function() {
 
+  sort: function(columnIndex) {
+    var rows = TableSorter.getRows();
+    return Quicksort.sort(rows,columnIndex)
   },
+
+  getRows: function() {
+    return $('tbody tr')
+  }
+
 }
